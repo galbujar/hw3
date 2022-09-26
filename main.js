607e1e82afcb5c92e9e1a23e48d14acac3ae2466
⚡ ⚡ <galbujar@mail.valenciacollege.edu>
2022-09-25 23:56:14 -0400

    almost

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+let userChoice = prompt("heads or tails?")
+let randomNumber = Math.floor(Math.random()*2)? 0 : 1;
+console.log(randomNumber)
+let computerChoice = randomNumber
+if (randomNumber === 0){
+    computerChoice = "heads";
+}else{
+    computerChoice = "tails";
+}
+console.log(computerChoice)
+
+if (userChoice = computerChoice){
+    alert("You guessed right! The coin flip landed on " + userChoice);
+}else if (userChoice != computerChoice){
+    alert("Sorry, the coin flip landed on " + !userChoice);
+}
+
 let birthYear = prompt("What year were you born in?")
 let currentYear = 2022
 let age = (currentYear - birthYear)
@@ -7,24 +24,4 @@ if (age >= 22){
     alert("Sorry, you are not old enough to drink in the US");
 } else {
     alert("You are old enough to drink in the US...barely")
-}
-
-
-
-
-prompt("heads or tails?")
-let userChoice = "heads" || "tails"
-let randomNumber = Math.floor(Math.random()*2)? 0 : 1;
-console.log(randomNumber)
-let computerChoice = "heads";
-if (randomNumber === 0)
-    randomNumber = computerChoice
-  else
-    randomNumber = computerChoice = "tails"
-    console.log(computerChoice)
-
-if (userChoice = computerChoice){
-    alert("You guessed right! The coin flip landed on " + userChoice);
-}else if (userChoice != computerChoice){
-    alert("Sorry, the coin flip landed on " + !userChoice);
 }
